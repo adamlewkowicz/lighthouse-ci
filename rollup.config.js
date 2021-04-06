b601f2a18afae50a17936f4b1430363bd39ac399
alk831 <adam54111@gmail.com>
2021-04-06 13:22:04 +0200

    update rollup builder

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/main.ts',
+  output: {
+    dir: './lib',
+    format: 'cjs',
+  },
+  plugins: [json(), typescript(), resolve(), commonjs()],
+};
