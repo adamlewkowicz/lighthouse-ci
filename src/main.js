0ada064842de8ae91b5fc7563080b75128d52acf
alk831 <adam54111@gmail.com>
2021-04-06 15:17:04 +0200

    build

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import { getOctokit } from '@actions/github';
-import {
-  buildAndServe,
-  checkoutBaseBranch,
-  createComment,
-  getActionInputs,
-  getLighthouseResult,
-  installDependencies,
-} from './utils';
+// import { getOctokit } from '@actions/github';
+// import {
+//   buildAndServe,
+//   checkoutBaseBranch,
+//   createComment,
+//   getActionInputs,
+//   getLighthouseResult,
+//   installDependencies,
+// } from './utils';
+
+const { getInput } = require('@actions/core');
 
 async function run() {
-  const { urls, token } = await getActionInputs();
-  console.log({ urls, token });
+  // const { urls, token } = await getActionInputs();
+  console.log({ urls, token: getInput('repo-token') });
 
   // const octokit = getOctokit(token)
   // await installDependencies()
