6d0db86e9ded9662a1f4f27c5d09fd3258581534
alk831 <adam54111@gmail.com>
2021-04-07 12:35:10 +0200

    update

--- dist/main.js
+++ dist/main.js
@@ -10,7 +10,7 @@ async function run() {
     // const lighthouseResultCurrent = await getLighthouseResult(urls[0]);
     const lighthouseResultCurrent = await utils_1.getLighthouseResult('https://amaro.com/br/pt/');
     //
-    console.log(Object.keys(lighthouseResultCurrent), lighthouseResultCurrent.lhr);
+    console.log(Object.keys(lighthouseResultCurrent), lighthouseResultCurrent.lhr, lighthouseResultCurrent);
     // await checkoutBaseBranch()
     // await installDependencies()
     // await buildAndServe()
